ac80693d8fb74f4e0805b9ae1221198990cdfa93
ejga2001 <enriquejga2021@gmail.com>
2025-04-20 01:06:10 +0200

    Updated scripts/run-fig15-pflat.sh so that it can run fig15 test with the appropiate parameters based on the kernel is currently running on

--- scripts/run-fig15-pflat.sh
+++ scripts/run-fig15-pflat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Fault latency with different sizes. Requiresa pftrace (CONFIG_PFTRACE)
+# Fault latency with different sizes. Requires pftrace (CONFIG_PFTRACE)
 # enabled kernel.
 
 set -o pipefail -o errexit
@@ -30,29 +30,31 @@ pushd "${BASE}"
 # FIXME: Create a proper CLI arg parser
 # pflag <VMA size in bytes> <nr of VMAs> <nr of threads> <THP-flag> <shuffle-flag (fault VMA randomly, not sequentially)> <nr of faults> <VMA # drop-flag (count VMA unmapping / flushing in the tracing)>
 
-# 4KiB (for 64KiB boot a kernel with a 64KiB granule -- CONFIG_ARM64_64K_PAGES)
-ok "Running pflat 4KiB..."
-pflat $(( 100 << 30 )) 1 1 false true 100000 false
-pflat.sh show
-pflat.sh clear
-exit
-
-# 64KiB ET faults
-ok "Running pflat 64KiB ET..."
-MODE=etheap ETHEAP=1 prctl.sh pflat $(( 100 << 30 )) 1 1 false true 100000 false
-pflat.sh show
-pflat.sh clear
-exit
-
-# 2MiB (for 32MiB boot a kernel with a 16KiB granule -- CONFIG_ARM64_16K_PAGES)
-ok "Running pflat 2MiB..."
-pflat $(( 100 << 30 )) 1 1 true true 100000 false
-pflat.sh show
-pflat.sh clear
-exit
-
-# 32MiB ET faults
-ok "Running pflat 32MiB ET..."
-MODE=etheap ETHEAP=1 prctl.sh prctl --ca --et -- pflat $(( 100 << 30 )) 1 1 true true 100000 false
-pflat.sh show
-pflat.sh clear
+if [ $(uname -r) == "6.8.12-v8+" ] \
+  || [ $(uname -r) == "6.8.12-v8+64k+" ]; then
+  # 4KiB (for 64KiB boot a kernel with a 64KiB granule -- CONFIG_ARM64_64K_PAGES)
+  ok "Running pflat 4KiB..."
+  pflat $(( 100 << 30 )) 1 1 false true 100000 false
+  pflat.sh show
+  pflat.sh clear
+elif [ $(uname -r) == "5.18.19-et+pftrace+" ]; then
+  # 64KiB ET faults
+  ok "Running pflat 64KiB ET..."
+  MODE=etheap ETHEAP=1 prctl.sh pflat $(( 100 << 30 )) 1 1 false true 100000 false
+  pflat.sh show
+  pflat.sh clear
+  # 32MiB ET faults
+  ok "Running pflat 32MiB ET..."
+  MODE=etheap ETHEAP=1 prctl.sh prctl --ca --et -- pflat $(( 100 << 30 )) 1 1 true true 100000 false
+  pflat.sh show
+  pflat.sh clear
+elif [ $(uname -r) == "5.18.19-vanilla+pftrace+4k+" ] \
+  || [ $(uname -r) == "5.18.19-vanilla+pftrace+16k+" ]; then
+  # 2MiB (for 32MiB boot a kernel with a 16KiB granule -- CONFIG_ARM64_16K_PAGES)
+  ok "Running pflat 2MiB..."
+  pflat $(( 100 << 30 )) 1 1 true true 100000 false
+  pflat.sh show
+  pflat.sh clear
+else
+  echo "ERROR: kernel $(uname -r) is not valid"
+fi
