c7af0c9c895d3b6c8cdc5e3374e102444f2e3958
ejga2001 <enriquejga2021@gmail.com>
2025-04-30 01:18:54 +0200

    NO_LIBPERL=1. There is a discrepancy between the perl from the host and the one from the VM.

--- scripts/build.sh
+++ scripts/build.sh
@@ -65,7 +65,7 @@ build_kernel() {
 	make -j$(nproc) Image.gz modules dtbs
 
 	ok "Building perf..."
-	make -j$(nproc) -C tools/perf WERROR=0 NO_LIBPERF
+	make -j$(nproc) -C tools/perf WERROR=0 NO_LIBPERF=1
 
 	popd
 }
